56c73eb23532419118787ec7858ca218e0d7a816
Jaime <Jaime@HYRULE-5500X>
2019-01-15 19:47:04 +0100

    APARTADOS 2 Y 3 // Actualizado README.md + Creado Enumerado Tramo y sus constantes

--- src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
@@ -2,7 +2,8 @@ package org.iesalandalus.programacion.reservasaulas;
 
 public class MainApp {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+        {
 		System.out.println("Programa para la gestión de reservas de espacios del IES Al-Ándalus");
 	}
 
